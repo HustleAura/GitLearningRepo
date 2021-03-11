e001b570f36627c0b71f79a2c215b71d3bf4c4a3
Manjeet Pani <manjeetpani@gmail.com>
2021-03-11 11:12:01 +0530

    Second commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,8 @@ public class HelloWorld {
 
 	public static void main(Strings[] args) throws exception {
 		
-		System.out.println("Indian style Hello World. Namaste World")
+		System.out.println("Indian style Hello World. Namaste World");
+		
+		System.out.println("This is the edit done to the file after the first commit");
 	}
 }
